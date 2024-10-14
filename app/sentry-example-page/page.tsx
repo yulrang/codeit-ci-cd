f12b92ed57ae168d1848687c05e062344e21a428
Lee Youl <2yulrang@gmail.com>
2024-10-14 18:01:32 +0900

    sentry test

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -49,15 +49,7 @@ export default function Page() {
             margin: "18px",
           }}
           onClick={async () => {
-            await Sentry.startSpan({
-              name: 'Example Frontend Span',
-              op: 'test'
-            }, async () => {
-              const res = await fetch("/api/sentry-example-api");
-              if (!res.ok) {
-                throw new Error("Sentry Example Frontend Error");
-              }
-            });
+            await Sentry.captureMessage("Something went wrong","error");
           }}
         >
           Throw error!
